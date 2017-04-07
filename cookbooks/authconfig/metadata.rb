be3fb50be05c3f1f64d92431f8efb3fbdce70eaa
zhengc <zhengc@pdf.com>
2017-04-07 13:38:41 +0800

    test

---
+++ cookbooks/authconfig/metadata.rb
@@ -0,0 +1,7 @@
+name             "authconfig"
+maintainer       "Jesse Campbell"
+maintainer_email "hikeit@gmail.com"
+license          "Apache License"
+description      "Configures authconfig"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "2.0.1"
