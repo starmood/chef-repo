d229265d96c448cc92007ac36ebee5ddcf5a7c8e
zhengc <zhengc@pdf.com>
2017-04-10 17:13:30 +0800

    test

--- cookbooks/PDF-snmpd/recipes/snmpd_centos.rb
+++ cookbooks/PDF-snmpd/recipes/snmpd_centos.rb
@@ -1,4 +1,20 @@
+
+
+%w{net-snmp}.each do |package|
+ 
+  package package do
+    action :install
+  end
+end
+
 template "/etc/snmp/snmpd.conf" do
         source PDF-snmpd-centos.erb
         mode "0644"
+end
+
+%w{snmpd}.each do |service|
+ 
+  service service do
+    action [ :enable, :start ]
+  end
 end
