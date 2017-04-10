6d0dd80e352405853a68bcfeedb26ff8eb30bf88
zhengc <zhengc@pdf.com>
2017-04-10 12:12:27 +0800

    test

--- cookbooks/PDF-autofs-pvg/recipes/default.rb
+++ cookbooks/PDF-autofs-pvg/recipes/default.rb
@@ -3,3 +3,22 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+%w{autofs nfs-utils nfs-utils-lib rpcbind}.each do |package|
+ 
+  package package do
+    action :install
+  end
+end
+
+cookbook_file "/etc/auto.master" do
+        source "auto.master"
+        mode "0644"
+end
+
+%w{autofs rpcbind nfs}.each do |service|
+ 
+  service service do
+    action [ :enable, :start ]
+  end
+end
