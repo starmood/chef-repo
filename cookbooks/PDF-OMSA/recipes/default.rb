172f6ce92af078fb0f9e541f4b71132bffb7975e
zhengc <zhengc@pdf.com>
2017-04-11 17:14:48 +0800

    test

--- cookbooks/PDF-OMSA/recipes/default.rb
+++ cookbooks/PDF-OMSA/recipes/default.rb
@@ -4,7 +4,21 @@
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
-template "/etc/yum.repos.d/dell-system-update.repo" do
-        source "yum-repo-dell.erb"
-        mode "0644"
+case node['platform']
+when 'redhat', 'centos'
+        case node['platform_version'].to_i
+        when 6,7
+		template "/etc/yum.repos.d/dell-system-update.repo" do
+		        source "yum-repo-dell.erb"
+		        mode "0644"
+		end
+	else 
+                Chef::Log.info( "PDF-Nagios: Only support Redhat/CentOS version 6,7 at this time." )
+                return
+	end
+
+else
+        Chef::Log.info( "PDF-Nagios: Only Redhat-based systems are supported at this time." )
+        return
+
 end
