58b835ec24cd50456447264393a9c91659a1ac4b
zhengc <zhengc@pdf.com>
2017-04-14 15:13:21 +0800

    test

--- cookbooks/PDF-generic/recipes/default.rb
+++ cookbooks/PDF-generic/recipes/default.rb
@@ -5,6 +5,7 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 
 ### 1. Disable services
+
 services = node["PDF-generic"]["disable_services"]
 
 services.each do |service|
@@ -16,3 +17,15 @@ end
 
 
 ### 2. Add alias
+
+aliases = node["PDF-generic"]["mail_alias"]
+
+aliases.each do |alias|
+	ruby_block "insert_line" do
+	  block do
+	    file = Chef::Util::FileEdit.new("/etc/aliases")
+	    file.insert_line_if_no_match(/#{alias}/, #{alias})
+	    file.write_file
+	  end
+	end
+end
