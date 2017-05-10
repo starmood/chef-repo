dbccbbbb868360243dc32db3ef54cc3bf1772b53
zhengc <zhengc@pdf.com>
2017-05-10 11:14:30 +0800

    selinux

--- cookbooks/PDF-selinux/recipes/default.rb
+++ cookbooks/PDF-selinux/recipes/default.rb
@@ -7,6 +7,6 @@
 include_recipe "selinux"
 
 Chef::Log.info('**************************************************************************')
-Chef::Log.info('If you disable SELINUX, you must reboot server first to make it effective!")
+Chef::Log.info('If you disable SELINUX, you must reboot server first to make it effective!')
 Chef::Log.info('**************************************************************************')
 
