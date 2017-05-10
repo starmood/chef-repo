87f533295e4cf604055eb2aa9d40d3e1408834fc
zhengc <zhengc@pdf.com>
2017-05-10 16:05:25 +0800

    rsyslog

---
+++ cookbooks/PDF-rsyslog/README.md
@@ -0,0 +1,4 @@
+# PDF-rsyslog
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/PDF-rsyslog/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: PDF-rsyslog
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
