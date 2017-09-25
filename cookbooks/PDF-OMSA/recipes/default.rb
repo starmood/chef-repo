8332d211a9864fc50d494f4e051bf63c85fba88c
zhengc <zhengc@pdf.com>
2017-09-25 16:35:52 +0800

    add file insert block for snmpd.conf in PDF_OSMA recipes, to fixed the issue if run chef-client twice, line for OSMA will be missed

--- cookbooks/PDF-OMSA/recipes/default.rb
+++ cookbooks/PDF-OMSA/recipes/default.rb
@@ -26,6 +26,15 @@ when 'redhat', 'centos'
 			timeout 5400
 			notifies :run, 'execute[start_omsa]', :immediately
                 end
+			
+		# if run chef-client twice, snmp cookbook will overwrite snmpd.conf again, and the line for OSMA will be missed.
+		ruby_block "insert_line" do
+		  block do
+		    file = Chef::Util::FileEdit.new("/etc/snmp/snmpd.conf")
+		    file.insert_line_if_no_match(/smuxpeer .1.3.6.1.4.1.674.10892.1/, "# Allow Systems Management Data Engine SNMP to connect to snmpd using SMUX\nsmuxpeer .1.3.6.1.4.1.674.10892.1")
+		    file.write_file
+		  end
+		end
 
     		execute 'start_omsa' do
     			command '/opt/dell/srvadmin/sbin/srvadmin-services.sh start'
